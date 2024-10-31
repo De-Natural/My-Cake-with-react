b3dac12d8890da4259e3022f89ca4e455d7bc51d
De-natural <festuschidubem376@gmail.com>
2024-10-31 13:27:53 -0700

    updating

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -59,26 +59,6 @@ export default function Dashboard() {
     setImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
   };
 
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.innerHTML = `
-      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-      (function(){
-      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-      s1.async=true;
-      s1.src='https://embed.tawk.to/672367074304e3196adb1ad3/1ibh624a6';
-      s1.charset='UTF-8';
-      s1.setAttribute('crossorigin','*');
-      s0.parentNode.insertBefore(s1,s0);
-      })();
-    `;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <div className="main">
       <div className="title">
--- src/components/Leftside.jsx
+++ src/components/Leftside.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import React, { useState, useEffect } from 'react';
 
 export default function Leftside({ imageData = {} }) {
   // Default values for properties in case imageData is undefined or missing some properties
@@ -10,26 +9,6 @@ export default function Leftside({ imageData = {} }) {
     text = '',
     profileImage = '/assets/pngwing-2-strawberry-half-cake.png'
   } = imageData || {}; // Fallback to an empty object if imageData is undefined
-
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.innerHTML = `
-      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-      (function(){
-      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-      s1.async=true;
-      s1.src='https://embed.tawk.to/672367074304e3196adb1ad3/1ibh624a6';
-      s1.charset='UTF-8';
-      s1.setAttribute('crossorigin','*');
-      s0.parentNode.insertBefore(s1,s0);
-      })();
-    `;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
 
   return (
     <div 
